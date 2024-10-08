1601765c93a39c6060ff87891a9d84b0bd45523f
SuperMegacy <pilusaemanuel@gmail.com>
2024-10-08 22:37:26 +0200

    conatct info

---
+++ PhoneBook_project/contact.py
@@ -0,0 +1,7 @@
+class Contact:
+    def __init__(self, name, phone):
+        self.name = name
+        self.phone = phone
+
+    def __repr_(self):
+        return f"Contact(Name: {self.name}}, Phone: {self.phone}"
